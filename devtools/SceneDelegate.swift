af002492447d237fd735064911154d10290dbfff
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2025-12-05 11:57:58 +0200

    Initial Commit

---
+++ devtools/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  devtools
+//
+//  Created by Stephan on 2025/12/05.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ devtoolsTests/devtoolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  devtoolsTests.swift
+//  devtoolsTests
+//
+//  Created by Stephan on 2025/12/05.
+//
+
+import Testing
+@testable import devtools
+
+struct devtoolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
